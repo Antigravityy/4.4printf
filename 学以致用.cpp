d182a9e956ed18d9afe91bf5702cd2517d1a7b3d
数数然 <143727361+Antigravityy@users.noreply.github.com>
2023-09-13 22:16:41 +0800

    Add files via upload

---
+++ 学以致用.cpp
@@ -0,0 +1,10 @@
+/*Ñ§ÒÔÖÂÓÃ*/
+#include<stdio.h>
+#define NAME "shuoshuoran"
+int main(void)
+{
+	float cash = 123456.123;
+	printf("The %s family just may be $%.2f dollars richer!\n", NAME, cash);
+
+	return 0;
+}
